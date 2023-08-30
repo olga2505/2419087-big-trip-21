b62dd866a6622d9de87a455c33e80507ab18a2dd
Olga Polyanskaya <151590@mail.ru>
2023-08-30 19:00:36 +0300

    Добавляет классам приватные поля

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,40 +3,48 @@ import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 import SortView from '../view/sort-view.js';
 import EventListView from '../view/event-list-view.js';
+
 export default class BoardPresenter {
-  sortComponent = new SortView();
-  eventListComponent = new EventListView();
+  #container = null;
+  #destinationsModel = null;
+  #offersModel = null;
+  #pointsModel = null;
+  #points = null;
+
+  #sortComponent = new SortView();
+  #eventListComponent = new EventListView();
 
   constructor({container, destinationsModel, offersModel, pointsModel}) {
-    this.container = container;
-    this.destinationsModel = destinationsModel;
-    this.offersModel = offersModel;
-    this.pointsModel = pointsModel;
+    this.#container = container;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
+    this.#pointsModel = pointsModel;
 
-    this.points = [...pointsModel.get()];
+    // this.#points = [...pointsModel.get()];
   }
 
   init() {
-    render(this.sortComponent, this.container);
-    render(this.eventListComponent, this.container);
+    this.#points = [...this.#pointsModel.get()];
+    render(this.#sortComponent, this.#container);
+    render(this.#eventListComponent, this.#container);
 
     render(
       new PointEditView({
         // point: this.points[0],
-        pointDestinations: this.destinationsModel.get(), // ???
-        pointOffers: this.offersModel.get(),
+        pointDestinations: this.#destinationsModel.get(), // ???
+        pointOffers: this.#offersModel.get(),
       }),
-      this.eventListComponent.element
+      this.#eventListComponent.element
     );
 
-    this.points.forEach((point) => {
+    this.#points.forEach((point) => {
       render(
         new PointView({
           point,
-          pointDestinations: this.destinationsModel.getById(point.destination),
-          pointOffers: this.offersModel.getByType(point.type),
+          pointDestinations: this.#destinationsModel.getById(point.destination),
+          pointOffers: this.#offersModel.getByType(point.type),
         }),
-        this.eventListComponent.element
+        this.#eventListComponent.element
       );
     });
   }
